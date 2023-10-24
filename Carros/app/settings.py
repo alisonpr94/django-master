5e5f93fa59630cde571aedd2855139754c391d53
Alison Pereira Ribeiro <alisonrib17@gmail.com>
2023-10-24 15:09:38 -0300

    Configurando postgres

--- Carros/app/settings.py
+++ Carros/app/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'carros',
+        'USER': 'postgres',
+        'PASSWORD': 'Lisonzer@17',
+        'HOT': 'localhost',
+        'PORT': '5432',
     }
 }
 
